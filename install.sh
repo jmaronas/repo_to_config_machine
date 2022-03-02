75fb5d799d54da5337d3932c3222a360d2b5ef48
Juan Maroñas Molano <jmaronasm@gmail.com>
2022-03-02 17:26:18 +0100

    .

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
-  exit
+  exit 1
 fi
 
 if [ $# -eq 0 ]
@@ -11,7 +11,6 @@ then
 fi
 
 conda_path=$1
-
 
 echo "I am exiting the script for the following reason: this config files might need you to configure first a VPN or any other stuff since your config files might execute ssh in protected networks. Once
 you are sure you have make all this set up that is hard do do within a general script (you might have different VPNs etc) then remove this sentence and the following exit. Also remember to sudo apt-get install git before running this since you are likely to have your set up in a github repository."
@@ -26,7 +25,7 @@ apt-get --purge autoremove -y
 apt-get autoclean
 
 ## Install ubuntu packages
-apt-get install -y vim git openssh-server curl trash-cli git-extras thunderbird network-manager-vpnc-gnome deja-dup
+apt-get install -y vim git openssh-server curl trash-cli git-extras thunderbird network-manager-vpnc-gnome deja-dup usb-creator-gtk gparted
 
 if [ "$?" -eq "1" ]; then
     echo "The apt-get install failed"
@@ -34,6 +33,16 @@ if [ "$?" -eq "1" ]; then
 fi
 
 ## Install packages not directly comming from apt-get
+
+# R
+add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu focal-cran40/'
+apt update
+apt install r-base -y
+
+if [ "$?" -eq "1" ]; then
+    echo "Installing R failed"
+    exit 1
+fi
 
 # skype
 wget  https://go.skype.com/skypeforlinux-64.deb -O /tmp/skypeforlinux-64.deb
@@ -50,6 +59,26 @@ apt install /tmp/zoom_amd64.deb -y
 
 if [ "$?" -eq "1" ]; then
     echo "Installing zoom failed"
+    exit 1
+fi
+
+# teams
+wget https://packages.microsoft.com/repos/ms-teams/pool/main/t/teams/teams_1.3.00.5153_amd64.deb -O /tmp/teams.deb 
+apt install /tmp/teams.deb -y
+
+if [ "$?" -eq "1" ]; then
+    echo "Installing teams failed"
+    exit 1
+fi
+
+# spotify
+curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | sudo apt-key add - 
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+apt update
+apt install spotify-client -y
+
+if [ "$?" -eq "1" ]; then
+    echo "Installing spotify failed"
     exit 1
 fi
 
